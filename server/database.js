aa0295610443bedc8b6af30d3f56130dbf83692b
Jayakanth Arun <jayakantharun03@gmail.com>
2023-10-04 13:52:40 +0530

    Updated db

--- server/database.js
+++ server/database.js
@@ -1,11 +1,15 @@
 const {Client} = require('pg')
 
+// const db = new Client({
+//     host:'localhost',
+//     port:5432,
+//     user:'postgres',
+//     password:'1234',
+//     database:'project'
+// })
+
 const db = new Client({
-    host:'localhost',
-    port:5432,
-    user:'postgres',
-    password:'1234',
-    database:'project'
+    connectionString : 'postgres://jayakanth:wVHhAbpUsrvHOHP0bpPgawdeEjZ44LyD@dpg-ckemjgunpffc738su3r0-a.oregon-postgres.render.com/dbmsproject?sslmode=verify-full',
 })
 
 db.on("connect",()=>{
